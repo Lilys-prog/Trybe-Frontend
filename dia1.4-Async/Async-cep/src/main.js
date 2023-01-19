27dbf34e4f2957283426e772ccf13bc3b4364fdf
Lily Silva <lilys.prog@gmail.com>
2023-01-19 17:53:32 -0300

    resolviido cep como no víideo

--- dia1.4-Async/Async-cep/src/main.js
+++ dia1.4-Async/Async-cep/src/main.js
@@ -1,31 +1,16 @@
-import fetch from 'node-fetch';
+import getAdress from "./getAdress"; 
 
-const cepValue = document.querySelector('#cep');
 const btn = document.querySelector('#consCEP');
-const endDados = document.querySelector('#result');
 
-
-// ESTÁ IGUAL AO SITE, MAS NÃO ESTÁ FUNCIONANDO (TEM QUE USAR O SIITE STACKBLITZ - JS)
-// const cep = '30130-010'; 
-// fetch(`https://viacep.com.br/ws/${cep}/json/`)
-//   .then(response => response.json())
-//   .then(data => console.log(data));
-
-// =============================================================
-
-// ASYNC/AWAIT
-
-const fetchCep = async (cep) => {
-  cep = cepValue.value;
+export async function handleClick() {
+  const cepValue = document.querySelector('#cep').value;
 
   try {
-    const api = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
-    const data = await api.json();
-    endDados.innerHTML = JSON.stringify(data);
-
+    const adressData = await getAdress(cepValue);
+    document.querySelector('#result').innerHTML = JSON.stringify(adressData);
   } catch (error) {
-    return error.message;
+    alert(error.message);
   }
 }
 
-btn.addEventListener('click', (() => fetchCep));
+btn.addEventListener('click', handleClick);
