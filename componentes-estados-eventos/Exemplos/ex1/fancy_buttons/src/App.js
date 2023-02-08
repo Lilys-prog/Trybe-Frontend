1f9619501450ecb25674f4c165454ac0f8570b75
Lily Silva <lilys.prog@gmail.com>
2023-02-08 15:33:03 -0300

    adicionando para o hub

--- componentes-estados-eventos/Exemplos/ex1/fancy_buttons/src/App.js
+++ componentes-estados-eventos/Exemplos/ex1/fancy_buttons/src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import './App.css';
 
+function handleClick() {
+  if (button.id === 'btn1')
+    console.log('Clicou');
+}
 class App extends React.Components() {
   render() {
     return (
       <main>
-        <button>Botão 1</button>;
-        <button>Botão 2</button>;
-        <button>Botão 3</button>;
+        <button id="btn1">Botão 1</button>;
+        <button id="btn2">Botão 2</button>;
+        <button id="btn3">Botão 3</button>;
       </main>
     )
   }
