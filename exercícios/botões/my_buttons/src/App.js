17c83e549716db47eabd043b7b2f3bbb1679bc10
Lily Silva <lilys.prog@gmail.com>
2023-03-03 12:01:25 -0300

    exercício do 3.1 - fancy buttons

--- exercícios/botões/my_buttons/src/App.js
+++ exercícios/botões/my_buttons/src/App.js
@@ -1,17 +1,60 @@
 import React from 'react';
 import './App.css';
-
-function handleClick(phrase) {
-  console.log(phrase);
-}
-
 class App extends React.Component {
-  render() {
+
+  state = {
+    btn1: 0,
+    btn2: 0,
+    btn3: 0,
+  }
+
+  handleClickOne = () => {    
+    this.setState((pState) => ({
+      btn1: pState.btn1 + 1,         
+    }))
+  }
+
+  handleClickTwo = () => {    
+    this.setState((pState) => ({
+      btn2: pState.btn2 + 1,         
+    }))
+  }
+
+  handleClickThree = () => {    
+    this.setState((pState) => ({
+      btn3: pState.btn3 + 1,         
+    }))
+  }
+
+  getButtonColor = (num) => num % 2 === 0 ? 'green' : 'white';
+
+  render() {    
+  const {btn1, btn2, btn3} = this.state;
     return (
       <div className="App">
-        <button onClick={() => {handleClick('1')}}>Botão 1</button>
-        <button onClick={() => {handleClick('2')}}>Botão 2</button>
-        <button onClick={() => {handleClick('3')}}>Botão 3</button>        
+        <button
+        type="button"
+          onClick={this.handleClickOne}
+          style= { { backgroundColor: this.getButtonColor(btn1) } }
+        >
+          {btn1}
+        </button>
+
+        <button
+        type="button"
+          onClick={this.handleClickTwo}
+          style= { { backgroundColor: this.getButtonColor(btn2) } }
+        >
+          {btn2}
+        </button>
+
+        <button
+        type="button"
+          onClick={this.handleClickThree}
+          style= { { backgroundColor: this.getButtonColor(btn3) } }
+        >
+          {btn3}
+        </button>        
       </div>      
     );
   }
